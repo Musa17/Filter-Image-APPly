07544b408aaaca2a1da25e1aa6e9f3fc46031fd4
Musa17 <amusul00@fesb.hr>
2021-10-15 19:43:45 +0200

    Setting default values

--- Filtering Image/Filtering_Image.py
+++ Filtering Image/Filtering_Image.py
@@ -3,6 +3,10 @@ from PIL import Image
 import cv2
 
 def median_filter(data, filter_size):
+    temp = []
+    indexer = filter_size // 2
+    data_final = []
+    data_final = numpy.zeros((len(data),len(data[0])))
 
     return
 
