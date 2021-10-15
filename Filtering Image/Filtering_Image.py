543988782606eba9c1b854f9d1f5dbf0b335a700
Musa17 <amusul00@fesb.hr>
2021-10-15 19:49:56 +0200

    Making core functionality (Applying median filter to image)

--- Filtering Image/Filtering_Image.py
+++ Filtering Image/Filtering_Image.py
@@ -8,7 +8,25 @@ def median_filter(data, filter_size):
     data_final = []
     data_final = numpy.zeros((len(data),len(data[0])))
 
-    return
+    for i in range(len(data)):
+
+        for j in range(len(data[0])):
+
+            for z in range(filter_size):
+                if i + z - indexer < 0 or i + z - indexer > len(data) - 1:
+                    for c in range(filter_size):
+                        temp.append(0)
+                else:
+                    if j + z - indexer < 0 or j + indexer > len(data[0]) - 1:
+                        temp.append(0)
+                    else:
+                        for k in range(filter_size):
+                            temp.append(data[i + z - indexer][j + k - indexer])
+
+            temp.sort()
+            data_final[i][j] = temp[len(temp) // 2]
+            temp = []
+    return data_final
 
 
 def main():
